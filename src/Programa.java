cc9edf862edfaf1bf97ed2d91ec97fd4866b0977
Julio Zeferino <julioszeferino@gmail.com>
2023-11-06 19:11:52 -0300

    <feat> cria model Posicao

---
+++ src/Programa.java
@@ -0,0 +1,9 @@
+import Tabuleiro.Posicao;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Posicao posicao = new Posicao(3, 5);
+        System.out.println(posicao);
+    }
+}
