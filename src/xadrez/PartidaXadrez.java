5a8c3ae6ce8148dbf288c2e7d4ef115592740d88
Julio Zeferino <julioszeferino@gmail.com>
2023-11-06 19:48:01 -0300

    <feat> Criacao das primeiras pecas Rei e Torre

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,9 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
 
@@ -8,6 +11,7 @@ public class PartidaXadrez {
 
     public PartidaXadrez() {
         tabuleiro = new Tabuleiro(8, 8);
+        configInicial();
     }
 
     public PecaXadrez[][] getPecas() {
@@ -19,6 +23,12 @@ public class PartidaXadrez {
             }
         }
         return mat;
+    }
+
+    private void configInicial() {
+        tabuleiro.colocaPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
+        tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+        tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
     }
 
 }
